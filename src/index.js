93439944c624068df4fd848d9afe6274474e5a84
Lim Joe Quan <joequanlim@gmail.com>
2026-01-16 15:59:32 +0800

    Prepare imports, css file and tab eventlisteners

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import {} from './home.js'
+import {} from './menu.js'
+import {} from './about.js'
+import './styles.css'
+
+//event listeners for tab buttons
+let homeBtn = document.querySelector('#home')
+let menuBtn = document.querySelector('#menu')
+let aboutBtn = document.querySelector('#about')
+
+homeBtn.addEventListener('click', () => {
+    //load home page from homejs import
+})
+
+menuBtn.addEventListener('click', () => {
+    //load menu page from menujs import
+})
+
+aboutBtn.addEventListener('click', () => {
+    //load about page from aboutjs import
+})
