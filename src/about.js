34bb978c25106f9ee811ae44a9f68752dd077dd4
Lim Joe Quan <joequanlim@gmail.com>
2026-01-17 00:35:03 +0800

    Prepare code for beginnings section
    to do list:
     - Increase length of desc tc. mention discovering more friends and
       branching out from plushie village
     - Layout of beginnings section is simillar to hero section in home page
     - Complete rest of about section (plan out the layout before coding!)

--- src/about.js
+++ src/about.js
@@ -17,7 +17,29 @@ function createTitleSection() {
 }
 
 function createBeginningsSection() {
-    
+    let beginningsCard = document.createElement('div')
+    beginningsCard.classList.add('card')
+    let beginningsTitle = document.createElement('h2')
+    beginningsTitle.classList.add('section-title')
+    beginningsTitle.textContent = 'Beginnings'
+    beginningsCard.appendChild(beginningsTitle)
+    contentCard.appendChild(beginningsCard)
+    let beginningsContainer = document.createElement('div')
+    beginningsContainer.classList.add('beginnings', 'section-container')
+    let beginningsText = document.createElement('div')
+    beginningsText.classList.add('beginnings-text')
+    let beginningsDate = document.createElement('h3')
+    beginningsDate.classList.add('section-date')
+    beginningsDate.textContent = '2016'
+    beginningsText.appendChild(beginningsDate)
+    let beginningsDescription = document.createElement('p')
+    beginningsDescription.classList.add('section-text')
+    //Increase length of desc tc. mention discovering more friends and branching out from plushie village
+    beginningsDescription.textContent = "After coming back from a 1 year expedition to Taiwan, founder Pink Bear Bear decided to continue exploring, and set up a new Plushie Kingdom in Alor Setar."
+    beginningsText.appendChild(beginningsDescription)
+    beginningsContainer.appendChild(beginningsText)
+    beginningsCard.appendChild(beginningsContainer)
+    contentCard.appendChild(beginningsCard)
 }
 
 export function renderContent() {
